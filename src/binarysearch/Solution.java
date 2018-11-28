7d10948c255208c1290337f0f7812b67d1749827
9932xt <18813294167@163.com>
2018-11-28 21:12:21 +0800

    test

--- src/binarysearch/Solution.java
+++ src/binarysearch/Solution.java
@@ -181,6 +181,7 @@ public class Solution {
         return -1;
     }
 
+    // hh
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 6, 7, 7, 7, 7, 8, 9, 10};
         System.out.println(new Solution().getFirstEle(arr, 7));
