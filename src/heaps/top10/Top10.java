f035b87a5fab717ab2c8b3f4de5df2108a7fe0d3
xuwudong <1819228504@qq.com>
2019-01-17 22:59:28 +0800

    update

--- src/heaps/top10/Top10.java
+++ src/heaps/top10/Top10.java
@@ -1,71 +1,83 @@
 package heaps.top10;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Top10 {
-	private static File[] files = new File[10];
-	private static int count = 100000;
+    private static File[] files = new File[10];
+    private static int count = 1000;
+    private static Map<Integer, BufferedWriter> fileReaders = new HashMap<>();
 
-	private static String path = "D:/top10/";
+    private static String path = "D:/top10/";
 
-	public static void init() throws IOException {
-		initBigFile();
-		init10File();
-	}
+    public static File init() throws IOException {
+        File bigFile = initBigFile();
+        init10File();
+        return bigFile;
+    }
 
-	public static void initBigFile() throws IOException {
-		File file = new File(path);
-		if (!file.exists()) {
-			file.mkdirs();
-		}
-		File bigFile = new File(path + "bigFile.txt");
-		if (!bigFile.exists()) {
-			bigFile.createNewFile();
-		} else {
-			try (FileWriter fileWriter = new FileWriter(bigFile)) {
-				fileWriter.write("");
-				fileWriter.flush();
-			}
-		}
-		// 写入内容
-		writeContent(bigFile);
-	}
+    public static File initFile(File file) throws IOException {
+        if (!file.exists()) {
+            file.createNewFile();
+        } else {
+            try (FileWriter fileWriter = new FileWriter(file)) {
+                fileWriter.write("");
+                fileWriter.flush();
+            }
+        }
+        return file;
+    }
 
-	public static void writeContent(File file) throws IOException {
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-			for (int i = 0; i < count; i++) {
-				for (int j = 0; j < i; j++) {
-					writer.write(i + j + "\r\n");
-				}
-			}
-		}
-	}
+    public static File initBigFile() throws IOException {
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        File bigFile = new File(path + "bigFile.txt");
+        initFile(bigFile);
+        // 写入内容
+        writeContent(bigFile);
+        return bigFile;
+    }
 
-	public static void init10File() throws IOException {
-		for (int i = 0; i < 10; i++) {
-			File tmpfile = new File(path + "file" + i + ".txt");
-			if (!tmpfile.exists()) {
-				tmpfile.createNewFile();
-			} else {
-				try (FileWriter fileWriter = new FileWriter(tmpfile)) {
-					fileWriter.write("");
-					fileWriter.flush();
-				}
-			}
-		}
-	}
+    public static void writeContent(File file) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (int i = 0; i < count; i++) {
+                for (int j = 0; j < i; j++) {
+                    writer.write(i + j + "\r\n");
+                }
+            }
+        }
+    }
 
-	public static void top10(File file) {
-//		for(int )
-	}
+    public static void init10File() throws IOException {
+        for (int i = 0; i < 10; i++) {
+            File tmpFile = new File(path + "file" + i + ".txt");
+            tmpFile = initFile(tmpFile);
+            BufferedWriter br = new BufferedWriter(new FileWriter(tmpFile));
+            fileReaders.put(i, br);
+        }
+    }
 
-	public static void main(String[] args) throws IOException {
-		long start = System.currentTimeMillis();
-		init();
-		long end = System.currentTimeMillis();
-		System.out.println("spend time:" + (end - start) / 1000 + "s");
-	}
+    public static void top10(File bigFile) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(bigFile)))) {
+            String line = reader.readLine();
+            while (line != null && line != "\r\n") {
+                int n = Integer.valueOf(line) % 10;
+                BufferedWriter br = fileReaders.get(n);
+                br.write(line + "\r\n");
+                line = reader.readLine();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        File bigFile = init();
+        top10(bigFile);
+        long end = System.currentTimeMillis();
+        System.out.println("spend time:" + (end - start) / 1000 + "s");
+    }
 }
