43bfc2afa57a904bd44435b1cef8bcf4bab92a34
xuwudong <xuwudong@henhaoji.com>
2019-01-17 21:03:20 +0800

    update

--- src/heaps/top10/Top10.java
+++ src/heaps/top10/Top10.java
@@ -1,24 +1,71 @@
 package heaps.top10;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Top10 {
 	private static File[] files = new File[10];
+	private static int count = 100000;
 
 	private static String path = "D:/top10/";
-	public static void init() {
 
+	public static void init() throws IOException {
+		initBigFile();
+		init10File();
 	}
 
-	public static void initBigFile() {
+	public static void initBigFile() throws IOException {
 		File file = new File(path);
-		if(!file.exists()){
+		if (!file.exists()) {
 			file.mkdirs();
 		}
-		
+		File bigFile = new File(path + "bigFile.txt");
+		if (!bigFile.exists()) {
+			bigFile.createNewFile();
+		} else {
+			try (FileWriter fileWriter = new FileWriter(bigFile)) {
+				fileWriter.write("");
+				fileWriter.flush();
+			}
+		}
+		// 写入内容
+		writeContent(bigFile);
 	}
 
-	public static void main() {
+	public static void writeContent(File file) throws IOException {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+			for (int i = 0; i < count; i++) {
+				for (int j = 0; j < i; j++) {
+					writer.write(i + j + "\r\n");
+				}
+			}
+		}
+	}
 
+	public static void init10File() throws IOException {
+		for (int i = 0; i < 10; i++) {
+			File tmpfile = new File(path + "file" + i + ".txt");
+			if (!tmpfile.exists()) {
+				tmpfile.createNewFile();
+			} else {
+				try (FileWriter fileWriter = new FileWriter(tmpfile)) {
+					fileWriter.write("");
+					fileWriter.flush();
+				}
+			}
+		}
+	}
+
+	public static void top10(File file) {
+//		for(int )
+	}
+
+	public static void main(String[] args) throws IOException {
+		long start = System.currentTimeMillis();
+		init();
+		long end = System.currentTimeMillis();
+		System.out.println("spend time:" + (end - start) / 1000 + "s");
 	}
 }
