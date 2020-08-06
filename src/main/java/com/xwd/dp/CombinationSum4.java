c1fc7293f3a6a3517006ef52c5f8af447c3ba9f4
xuwudong <1819228504@qq.com>
2020-08-06 09:01:25 +0800

    组合求和 IV

---
+++ src/main/java/com/xwd/dp/CombinationSum4.java
@@ -0,0 +1,44 @@
+package com.xwd.dp;
+
+/**
+ * 377. 组合总和 Ⅳ
+ * 给定一个由正整数组成且不存在重复数字的数组，找出和为给定目标正整数的组合的个数。
+ *
+ * 示例:
+ *
+ * nums = [1, 2, 3]
+ * target = 4
+ *
+ * 所有可能的组合为：
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ *
+ * 请注意，顺序不同的序列被视作不同的组合。
+ *
+ * 因此输出为 7。
+ */
+public class CombinationSum4 {
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] memo = new int[target + 1];
+        memo[0] = 1;
+        for (int i = 0; i < target; i++) {
+            for (int num : nums) {
+                if (i + num <= target) {
+                    memo[i + num] += memo[i];
+                }
+            }
+        }
+        return memo[target];
+    }
+
+    public static void main(String[] args) {
+        CombinationSum4 combinationSum4 = new CombinationSum4();
+        System.out.println(combinationSum4.combinationSum4(new int[]{1, 2, 3}, 32));
+    }
+}
