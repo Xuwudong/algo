54cf063fda4bacffb810678df5019afbd2dc2b84
xuwudong <1819228504@qq.com>
2021-03-11 11:49:59 +0800

    feat:update

--- src/main/java/com/xwd/backtracking/Exist.java
+++ src/main/java/com/xwd/backtracking/Exist.java
@@ -8,14 +8,11 @@ package com.xwd.backtracking;
  **/
 public class Exist {
 
-    private boolean exist = false;
-
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 boolean[][] used = new boolean[board.length][board[i].length];
-                dfs(i, j, board, word, 0, used);
-                if (exist) {
+                if (dfs(i, j, board, word, 0, used)) {
                     return true;
                 }
             }
@@ -23,37 +20,66 @@ public class Exist {
         return false;
     }
 
-    private void dfs(int row, int col, char[][] board, String word, int index, boolean[][] used) {
-        if (board[row][col] == word.charAt(index)) {
-            used[row][col] = true;
-
-            if (index + 1 == word.length()) {
-                exist = true;
-                return;
-            }
-
-            if (row + 1 < board.length && !used[row + 1][col]) {
-                dfs(row + 1, col, board, word, index + 1, used);
-            }
-            if (col + 1 < board[row].length && !used[row][col + 1]) {
-                dfs(row, col + 1, board, word, index + 1, used);
-            }
-            if (row - 1 >= 0 && !used[row - 1][col]) {
-                dfs(row - 1, col, board, word, index + 1, used);
-            }
-            if (col - 1 >= 0 && !used[row][col - 1]) {
-                dfs(row, col - 1, board, word, index + 1, used);
-            }
-            used[row][col] = false;
+    private boolean dfs(int row, int col, char[][] board, String word, int index, boolean[][] used) {
+        if (index == word.length()) {
+            return true;
         }
+        if (row < 0 || row >= board.length || col < 0 || col >= board[row].length || board[row][col] != word.charAt(index) || used[row][col]) {
+            return false;
+        }
+        used[row][col] = true;
+
+        boolean res = dfs(row + 1, col, board, word, index + 1, used) ||
+                dfs(row, col + 1, board, word, index + 1, used) ||
+                dfs(row - 1, col, board, word, index + 1, used) ||
+                dfs(row, col - 1, board, word, index + 1, used);
+        used[row][col] = false;
+        return res;
     }
 
     public static void main(String[] args) {
-//        char[][] board = new char[][];
-        char[][] board = new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
-//        System.out.println(new Exist().exist(board, "a"));
-        System.out.println(new Exist().exist(board, "ABCCED"));
-        System.out.println(new Exist().exist(board, "SEE"));
-        System.out.println(new Exist().exist(board, "ABCB"));
+        char[][] board = new char[][]{{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a'}
+        ,{'a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','a','b'}};
+        System.out.println(new Exist().exist(board, "baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaa"));
+//        System.out.println(new Exist().exist(board, 'SEE'));
+//        System.out.println(new Exist().exist(board, 'ABCB'));
+//        char[][] board = new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+//        System.out.println(new Exist().exist(board, "ABCCED"));
     }
 }
