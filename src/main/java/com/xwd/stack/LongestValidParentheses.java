81c254ea3ae9081e5c61f4ef6bfae7782c7a6126
xuwudong <xuwudong@bigo.sg>
2021-04-12 21:02:09 +0800

    feat:update

---
+++ src/main/java/com/xwd/stack/LongestValidParentheses.java
@@ -0,0 +1,78 @@
+package com.xwd.stack;
+
+import java.util.Stack;
+
+//给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：s = "(()"
+//输出：2
+//解释：最长有效括号子串是 "()"
+//
+//
+// 示例 2：
+//
+//
+//输入：s = ")()())"
+//输出：4
+//解释：最长有效括号子串是 "()()"
+//
+//
+// 示例 3：
+//
+//
+//输入：s = ""
+//输出：0
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= s.length <= 3 * 104
+// s[i] 为 '(' 或 ')'
+//
+//
+//
+// Related Topics 字符串 动态规划
+// 👍 1263 👎 0
+
+
+
+/**
+ * @program: algo
+ * @description:
+ * @author: xuwudong
+ * @create: 2021-04-12 18:08
+ **/
+public class LongestValidParentheses {
+    public int longestValidParentheses(String s) {
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                }
+                max = Math.max(max, i - stack.peek());
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LongestValidParentheses main = new LongestValidParentheses();
+        System.out.println(main.longestValidParentheses(")()())"));
+    }
+}
