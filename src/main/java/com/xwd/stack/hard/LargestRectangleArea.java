a0f4c035dbf47d97cc035b580ed62f5c5635db54
xuwudong <xuwudong@bigo.sg>
2021-04-13 21:04:31 +0800

    feat:update

---
+++ src/main/java/com/xwd/stack/hard/LargestRectangleArea.java
@@ -0,0 +1,66 @@
+package com.xwd.stack.hard;
+
+import java.util.Stack;
+
+
+//给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+//
+// 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+//
+//
+//
+//
+//
+// 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+//
+//
+//
+//
+//
+// 图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+//
+//
+//
+// 示例:
+//
+// 输入: [2,1,5,6,2,3]
+//输出: 10
+// Related Topics 栈 数组 单调栈
+// 👍 1307 👎 0
+
+
+/**
+ * @program: algo
+ * @description:
+ * @author: xuwudong
+ * @create: 2021-04-13 20:59
+ **/
+public class LargestRectangleArea {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> monoStack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!monoStack.isEmpty() && heights[monoStack.peek()] >= heights[i]) {
+                monoStack.pop();
+            }
+            left[i] = monoStack.isEmpty() ? -1 : monoStack.peek();
+            monoStack.push(i);
+        }
+        monoStack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!monoStack.isEmpty() && heights[monoStack.peek()] >= heights[i]) {
+                monoStack.pop();
+            }
+            right[i] = monoStack.isEmpty() ? n : monoStack.peek();
+            monoStack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            // 计算面积
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+}
